f487072fd2ecc321419ae22c775aaf21ad66f6cd
sarwar-asik <sarwarasik@gmail.com>
2023-12-15 20:13:55 +0600

    moderator done

--- src/app/modules/student/student.controller.ts
+++ src/app/modules/student/student.controller.ts
@@ -1,16 +1,17 @@
 import { Request, Response } from 'express';
 import httpStatus from 'http-status';
-import { paginationFields } from '../../../constants/pagination';
-import catchAsync from '../../../shared/catchAsync';
-import pick from '../../../shared/pick';
-import sendResponse from '../../../shared/sendResponse';
+
 import { studentFilterableFields } from './student.constant';
 import { IStudent } from './student.interface';
 import { StudentService } from './student.service';
+import catchAsync from '../../share/catchAsync';
+import pick from '../../share/pick';
+import { PAGINATION_FIELDS } from '../../../constant/pagination';
+import sendResponse from '../../share/sendResponse';
 
 const getAllStudents = catchAsync(async (req: Request, res: Response) => {
   const filters = pick(req.query, studentFilterableFields);
-  const paginationOptions = pick(req.query, paginationFields);
+  const paginationOptions = pick(req.query, PAGINATION_FIELDS);
 
   const result = await StudentService.getAllStudents(
     filters,
