15b406c1e61b281f3ae872a5a6f6888639e6da59
sarwar-asik <sarwarasik@gmail.com>
2023-12-15 20:18:30 +0600

    moderator routes done

--- src/app/modules/student/student.route.ts
+++ src/app/modules/student/student.route.ts
@@ -1,19 +1,26 @@
 import express from 'express';
 
 import { StudentController } from './student.controller';
-import { StudentValidaion } from './student.validation';
+
 import validateRequestZod from '../../middlewares/validateRequestZod';
+import authMiddleware from '../../middlewares/authMiddleware';
+import { ENUM_USER_ROLE } from '../../../enums/users';
+import { StudentValidation } from './student.validation';
 const router = express.Router();
 
-router.get('/:id', StudentController.getSingleStudent);
-router.get('/', StudentController.getAllStudents);
+router
+  .route('/')
+  .get( authMiddleware(ENUM_USER_ROLE.ADMIN), StudentController.getAllStudents)
 
-router.delete('/:id', StudentController.deleteStudent);
 
-router.patch(
-  '/:id',
-  validateRequestZod(StudentValidaion.updateStudentZodSchema),
-  StudentController.updateStudent
-);
+router
+  .route('/:id')
+  .get( authMiddleware(ENUM_USER_ROLE.ADMIN,ENUM_USER_ROLE.student),StudentController.getSingleStudent)
+  .put(
+    authMiddleware(ENUM_USER_ROLE.ADMIN),
+    validateRequestZod(StudentValidation.updateStudentZodSchema),
+    StudentController.updateStudent
+  )
+  .delete(authMiddleware(ENUM_USER_ROLE.ADMIN), StudentController.deleteStudent);
 
 export const StudentRoutes = router;
--- src/app/modules/student/student.validation.ts
+++ src/app/modules/student/student.validation.ts
@@ -35,6 +35,6 @@ const updateStudentZodSchema = z.object({
   }),
 });
 
-export const StudentValidaion = {
+export const StudentValidation = {
   updateStudentZodSchema,
 };
