45f067e6d1441c0ea57f4cfb7914689deb116c01
sampod76 <sampodnath76@gmail.com>
2023-12-19 06:15:56 +0600

    remove dist file

--- dist/app/modules/lesson/lesson.constant.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.LESSON_FILTERABLE_FIELDS = exports.LESSON_SEARCHABLE_FIELDS = void 0;
-exports.LESSON_SEARCHABLE_FIELDS = ['title', 'details', "tags"];
-exports.LESSON_FILTERABLE_FIELDS = ['searchTerm', 'status', "select", "module"];
--- dist/app/modules/user/user.constant.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.userFilterableFields = exports.userSearchableFields = void 0;
-exports.userSearchableFields = ['email'];
-exports.userFilterableFields = ['searchTerm', 'role'];
